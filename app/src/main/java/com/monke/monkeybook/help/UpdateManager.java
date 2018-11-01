fbed70b96a98cd5660ab62d921c9f16e2913c8ed
Administrator <365650856@qq.com>
2018-11-01 20:01:05 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/help/UpdateManager.java
+++ app/src/main/java/com/monke/monkeybook/help/UpdateManager.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.widget.Toast;
@@ -135,4 +136,5 @@ public class UpdateManager {
     public static String getSavePath(String fileName) {
         return Environment.getExternalStoragePublicDirectory(DOWNLOAD_SERVICE).getPath() + fileName;
     }
+
 }
