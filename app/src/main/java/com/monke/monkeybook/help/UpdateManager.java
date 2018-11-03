4889a7be9ab17af03c95b0b36015801d0f1cd445
Administrator <365650856@qq.com>
2018-11-03 13:54:09 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/help/UpdateManager.java
+++ app/src/main/java/com/monke/monkeybook/help/UpdateManager.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.os.Handler;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.widget.Toast;
